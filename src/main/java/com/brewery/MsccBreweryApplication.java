39cda6c52de895d76ae9a8226f0c6983f2f50a12
sparkle <soroka.igor.olegovych@gmail.com>
2024-06-03 10:32:03 +0300

    init

---
+++ src/main/java/com/brewery/MsccBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsccBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsccBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/MsccBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
